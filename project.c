0f39639bd5ef1e0d1b82868f317b8cecddfccccd
BilalAS173 <bilalahmedshariff2005@gmail.com>
2024-11-17 00:33:16 +0500

    second commit

--- project.c
+++ project.c
@@ -1,8 +1,39 @@
 #include<stdio.h>
+struct Book {
+    char name[100];
+    int id;
+    char author[100];
+    int issuedstatus;
+} books[500];
+struct Member {
+    char name[100];
+    int id;
+    int contactnum[20];
+} Members[500];
 int main()
 {
+int choice;
+printf("Choose from the following options:\n 1- Issue book\n 2- Return book\n 3- Display all books \n 4-Add a Member\n 5- Exit\n");
+printf("Enter your choice: ");
+scanf("%d",&choice);
+switch (choice) {
+    case 1:
+    issuebook();
+    break;
+    case 2:
+    returnbook();
+    break;
+    case 3:
+    Displaybook();
+    break;
+     case 4:
+    addmember();
+    break;
+      case 5:
+    printf("Thank you for visiting our library management system. Please come again!\n");
+    break;
+}
   
-    int a=5;
-    printf("%d",a);
+
     return 0;
 }
