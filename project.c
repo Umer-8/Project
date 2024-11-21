35c842bb194101b7cea8d8e229d9931e16e24cac
BilalAS173 <bilalahmedshariff2005@gmail.com>
2024-11-21 23:44:59 +0500

    fourth commit

--- project.c
+++ project.c
@@ -1,5 +1,5 @@
  #include<stdio.h>
- 
+ #include <string.h>
 struct Book 
 {
     char title[100];
@@ -14,97 +14,78 @@ struct Member
     int id;
     int contactnum[20];
     int av=0;
+    char pass[40];
 } members[100];
 
 int loadbooks(struct Book books[40])
 {
-    FILE *ptr=fopen("books.txt","r");
+    FILE *fptr=fopen("books.txt","r");
   if(fptr==NULL)
  {
      printf("File does not exist \n");
      return 0;
  }
     int count=0;
-    while(fscanf(fptr,"%d,%[^,],%[^,],%[^,],%d\n",&books[count].id,books[count].titile,books[count].author,books[count].genre,&books[count].available)==4)
+    while(fscanf(fptr,"%d,%[^,],%[^,],%[^,],%d\n",&books[count].id,books[count].title,books[count].author,books[count].genre,&books[count].available)==5)
     {
     count ++;
     }
 fclose(fptr);
 return count;
 }
-void issuebook(struct Book books[40],int id,int count) ;
-{
-    int found=0;
-      for(int i=0;i<count;i++)
-      {
-         if(books[i].id…
-[10:30 AM, 11/21/2024] Umer: #include<stdio.h>
- 
-struct Book 
-{
-    char title[100];
-    int id;
-    char author[100];
-    int available=1;
-    char genre[100];
-} books[40];
-struct Member
-{
-    char name[100];
-    int id;
-    char pass[10];
-} members[100];
 
 int loadmembers(struct Member members[100])
 {
-    FILE *ptr=fopen("users.txt","r");
+    FILE *fptr=fopen("users.txt","r");
   if(fptr==NULL)
  {
      printf("File does not exist \n");
      return;
  }
  int count1=0;
-    while(fscanf(fptr,"%[^,],%d,%[^,]\n",&members[i].name,&members[i].id,members[i].pass)==3)
+    while(fscanf(fptr,"%49[^,],%d,%49[^,]\n",&members[count1].name,&members[count1].id,members[count1].pass)==3)
     {
-    count1 ++;
+    count1++;
     }
     fclose(fptr);
     return count1;
 }
-void login(struct Member members[100],int uid,char pass,int count1)
+void login(struct Member members[100],int uid,char upass,int count1)
 {
 for(int i=0;i<count1;i++)
 {
-    if(members[i].id==uid && members[i].pass==upass)
-    {
-        printf("Welcome to Library management system");
+    if(members[i].id==uid) {
+      if (strcmp(members[i].pass,upass)==0) {
+    printf("Welcome to Library management system");
+     return;
     }
     else
     {
-        printf("Invalid user ID or Password");
-        return;
+      printf("Invalid Password entered \n");
+    }
+    }
+    else {
+    printf("Invalid User ID entered. \t Please enter a valid ID\n");
+    break;
     }
 }
 }
-int loadbooks(struct Book books[40])
+void updatebooks(struct Book *books,int count)
 {
-    FILE *ptr=fopen("books.txt","r");
-  if(fptr==NULL)
- {
-     printf("File does not exist \n");
-     return 0;
- }
-    int count=0;
-    while(fscanf(fptr,"%d,%[^,],%[^,],%[^,],%d\n",&books[count].id,books[count].titile,books[count].author,books[count].genre,&books[count].available)==5)
-    {
-    count ++;
+    FILE *fptr=fopen("books.txt","w");
+      if(fptr==NULL)
+      {
+        printf("File does not exist \n");
+        return 0;
+      }
+  for(int i=0;i<count;i++)
+  {
+  fprintf(fptr,"%d,%s,%s,%s,%d\n",books[i].id,books[i].titile,books[i].author,books[i].genre,books[i].available);
     }
-fclose(fptr);
-return count;
+  fclose(fptr);
 }
-void issuebook(struct Book books[40],int id,int count) ;
-{
-    int found=0;
+void issuebook(struct Book books[40],int id,int count) {
+  int found=0;
       for(int i=0;i<count;i++)
       {
          if(books[i].id==id)
@@ -117,7 +98,7 @@ void issuebook(struct Book books[40],int id,int count) ;
             }
             else
             {
-                 printf("The book of ID %d and title %s by %s is already borrowed \n",books[i]id,books[i].title,books[i].author);
+            printf("The book of ID %d and title %s by %s is already borrowed \n",books[i].id,books[i].title,books[i].author);
                  found=1;
             }
             break;
@@ -127,24 +108,9 @@ void issuebook(struct Book books[40],int id,int count) ;
       {
         printf("You have entered wrong book ID \n");
       }
+      updatebooks(books,count);
 }
-void updatebooks(struct Book books[40],int count)
-{
-    FILE *ptr=fopen("books.txt","w");
-      if(fptr==NULL)
-      {
-        printf("File does not exist \n");
-        return 0;
-      }
-  for(int i=0;i<count;i++)
-  {
-      if (books[count].available==1)
-      {
-fprintf(fptr,"%d,%s,%s,%s,%d\n",books[count].id,books[count].titile,books[count].author,books[count].genre,books[count].available);
-      }
-  }
-fclose(fptr);
-}
+
 
  void  Displaybook(struct Book books[40]) 
  {
@@ -153,7 +119,7 @@ fclose(fptr);
   printf("ID Title Author\n");
        for (int i=0; i<count;i++) 
         {
-           if (books[i].isAvailable==1) 
+           if (books[i].available==1) 
            { 
             printf("%d  %s  %s\n", books[i].id, books[i].title, books[i].author);
            }
@@ -161,14 +127,14 @@ fclose(fptr);
  }
 int main()
 {
- int count1=loadmembers(members[100]);   
+ int count1=loadmembers(members);   
 int uid;
 int upass;
-printf("Enter User ID \n");
+printf("Enter your member ID: ");
 scanf("%d",&uid);
-printf("Enter User password \n");
+printf("Enter your password: ");
 scanf("%s",upass);
-login(members[100],uid,upass,count1);
+login(members,uid,upass,count1);
 
 int choice;
 printf("Choose from the following options:\n 1- Issue book\n 2- Return book\n 3- Display all books \n 4-Add a Member\n 5- Exit\n");
@@ -179,16 +145,13 @@ int id;
 printf("Enter Book ID \n");
 scanf("%d",&id);
 
-int count=0;
-
-int count=loadbooks(books[40]);
-
+int count=loadbooks(books);
 
 switch (choice) 
 {
      case 1:
-    issuebook(books[40],id,count);
-    updatebooks(books[40],count);
+     issuebook(books,id,count);
+    updatebooks(books,count);
     break;
     case 2:
     returnbook();
